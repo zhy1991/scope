753f5fd6528ed5f4f42cccf77fdbd28b4c56c114
Bryan Boreham <bjboreham@gmail.com>
2017-12-06 21:48:15 +0000

    Remove struct wrapper on Sets

--- report/sets.go
+++ report/sets.go
@@ -15,9 +15,7 @@ type stringSetEntry struct {
 }
 
 // Sets is a string->set-of-strings map.
-type Sets struct {
-	entries []stringSetEntry
-}
+type Sets []stringSetEntry
 
 // MakeSets returns EmptySets
 func MakeSets() Sets {
@@ -27,7 +25,7 @@ func MakeSets() Sets {
 // Keys returns the keys for this set
 func (s Sets) Keys() []string {
 	keys := make([]string, s.Size())
-	for i, v := range s.entries {
+	for i, v := range s {
 		keys[i] = v.key
 	}
 	return keys
@@ -35,99 +33,99 @@ func (s Sets) Keys() []string {
 
 // locate the position where key should go, either at the end or in the middle
 func (s Sets) locate(key string) int {
-	return sort.Search(len(s.entries), func(i int) bool {
-		return s.entries[i].key >= key
+	return sort.Search(len(s), func(i int) bool {
+		return s[i].key >= key
 	})
 }
 
 // Add the given value to the Sets, merging the contents if the key is already there.
 func (s Sets) Add(key string, value StringSet) Sets {
 	i := s.locate(key)
-	oldEntries := s.entries
-	if i == len(s.entries) {
-		s.entries = make([]stringSetEntry, len(oldEntries)+1)
-		copy(s.entries, oldEntries)
-	} else if s.entries[i].key == key {
-		value = value.Merge(s.entries[i].Value)
-		s.entries = make([]stringSetEntry, len(oldEntries))
-		copy(s.entries, oldEntries)
+	oldEntries := s
+	if i == len(s) {
+		s = make([]stringSetEntry, len(oldEntries)+1)
+		copy(s, oldEntries)
+	} else if s[i].key == key {
+		value = value.Merge(s[i].Value)
+		s = make([]stringSetEntry, len(oldEntries))
+		copy(s, oldEntries)
 	} else {
-		s.entries = make([]stringSetEntry, len(oldEntries)+1)
-		copy(s.entries, oldEntries[:i])
-		copy(s.entries[i+1:], oldEntries[i:])
+		s = make([]stringSetEntry, len(oldEntries)+1)
+		copy(s, oldEntries[:i])
+		copy(s[i+1:], oldEntries[i:])
 	}
-	s.entries[i] = stringSetEntry{key: key, Value: value}
+	s[i] = stringSetEntry{key: key, Value: value}
 	return s
 }
 
 // Delete the given set from the Sets.
 func (s Sets) Delete(key string) Sets {
 	i := s.locate(key)
-	if i == len(s.entries) || s.entries[i].key != key {
+	if i == len(s) || s[i].key != key {
 		return s // not found
 	}
-	result := make([]stringSetEntry, len(s.entries)-1)
+	result := make([]stringSetEntry, len(s)-1)
 	if i > 0 {
-		copy(result, s.entries[:i-1])
+		copy(result, s[:i-1])
 	}
-	copy(result[i:], s.entries[i+1:])
-	return Sets{entries: result}
+	copy(result[i:], s[i+1:])
+	return result
 }
 
 // Lookup returns the sets stored under key.
 func (s Sets) Lookup(key string) (StringSet, bool) {
 	i := s.locate(key)
-	if i < len(s.entries) && s.entries[i].key == key {
-		return s.entries[i].Value, true
+	if i < len(s) && s[i].key == key {
+		return s[i].Value, true
 	}
 	return MakeStringSet(), false
 }
 
 // Size returns the number of elements
 func (s Sets) Size() int {
-	return len(s.entries)
+	return len(s)
 }
 
 // Merge merges two sets maps into a fresh set, performing set-union merges as
 // appropriate.
 func (m Sets) Merge(n Sets) Sets {
 	switch {
-	case m.entries == nil:
+	case m == nil:
 		return n
-	case n.entries == nil:
+	case n == nil:
 		return m
 	}
-	l := len(m.entries)
-	if len(n.entries) > l {
-		l = len(n.entries)
+	l := len(m)
+	if len(n) > l {
+		l = len(n)
 	}
 	out := make([]stringSetEntry, 0, l+1)
 
 	i, j := 0, 0
-	for i < len(m.entries) {
+	for i < len(m) {
 		switch {
-		case j >= len(n.entries) || m.entries[i].key < n.entries[j].key:
-			out = append(out, m.entries[i])
+		case j >= len(n) || m[i].key < n[j].key:
+			out = append(out, m[i])
 			i++
-		case m.entries[i].key == n.entries[j].key:
-			newValue := m.entries[i].Value.Merge(n.entries[j].Value)
-			out = append(out, stringSetEntry{key: m.entries[i].key, Value: newValue})
+		case m[i].key == n[j].key:
+			newValue := m[i].Value.Merge(n[j].Value)
+			out = append(out, stringSetEntry{key: m[i].key, Value: newValue})
 			i++
 			j++
 		default:
-			out = append(out, n.entries[j])
+			out = append(out, n[j])
 			j++
 		}
 	}
-	for ; j < len(n.entries); j++ {
-		out = append(out, n.entries[j])
+	for ; j < len(n); j++ {
+		out = append(out, n[j])
 	}
-	return Sets{out}
+	return out
 }
 
 func (s Sets) String() string {
 	buf := bytes.NewBufferString("{")
-	for _, val := range s.entries {
+	for _, val := range s {
 		fmt.Fprintf(buf, "%s: %s,\n", val.key, val.Value)
 	}
 	fmt.Fprintf(buf, "}")
@@ -139,8 +137,8 @@ func (s Sets) DeepEqual(t Sets) bool {
 	if s.Size() != t.Size() {
 		return false
 	}
-	for i := range s.entries {
-		if !reflect.DeepEqual(s.entries[i], t.entries[i]) {
+	for i := range s {
+		if !reflect.DeepEqual(s[i], t[i]) {
 			return false
 		}
 	}
@@ -151,14 +149,14 @@ func (s Sets) DeepEqual(t Sets) bool {
 // Note this uses undocumented, internal APIs, which could break
 // in the future.  See https://github.com/weaveworks/scope/pull/1709
 // for more information.
-func (m *Sets) CodecEncodeSelf(encoder *codec.Encoder) {
+func (m Sets) CodecEncodeSelf(encoder *codec.Encoder) {
 	z, r := codec.GenHelperEncoder(encoder)
-	if m.entries == nil {
+	if m == nil {
 		r.EncodeNil()
 		return
 	}
 	r.EncodeMapStart(m.Size())
-	for _, val := range m.entries {
+	for _, val := range m {
 		z.EncSendContainerState(containerMapKey)
 		r.EncodeString(cUTF8, val.key)
 		z.EncSendContainerState(containerMapValue)
@@ -170,7 +168,7 @@ func (m *Sets) CodecEncodeSelf(encoder *codec.Encoder) {
 // CodecDecodeSelf implements codec.Selfer
 // Uses undocumented, internal APIs as for CodecEncodeSelf.
 func (m *Sets) CodecDecodeSelf(decoder *codec.Decoder) {
-	m.entries = nil
+	*m = nil
 	z, r := codec.GenHelperDecoder(decoder)
 	if r.TryDecodeAsNil() {
 		return
@@ -178,7 +176,7 @@ func (m *Sets) CodecDecodeSelf(decoder *codec.Decoder) {
 
 	length := r.ReadMapStart()
 	if length > 0 {
-		m.entries = make([]stringSetEntry, 0, length)
+		*m = make([]stringSetEntry, 0, length)
 	}
 	for i := 0; length < 0 || i < length; i++ {
 		if length < 0 && r.CheckBreak() {
@@ -190,15 +188,15 @@ func (m *Sets) CodecDecodeSelf(decoder *codec.Decoder) {
 			key = r.DecodeString()
 		}
 		j := m.locate(key)
-		if j == len(m.entries) || m.entries[j].key != key {
-			m.entries = append(m.entries, stringSetEntry{})
-			copy(m.entries[j+1:], m.entries[j:])
-			m.entries[j] = stringSetEntry{}
+		if j == len(*m) || (*m)[j].key != key {
+			*m = append(*m, stringSetEntry{})
+			copy((*m)[j+1:], (*m)[j:])
+			(*m)[j] = stringSetEntry{}
 		}
-		m.entries[j].key = key
+		(*m)[j].key = key
 		z.DecSendContainerState(containerMapValue)
 		if !r.TryDecodeAsNil() {
-			m.entries[j].Value.CodecDecodeSelf(decoder)
+			(*m)[j].Value.CodecDecodeSelf(decoder)
 		}
 	}
 	z.DecSendContainerState(containerMapEnd)
